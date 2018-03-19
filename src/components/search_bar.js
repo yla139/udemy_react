f752a2db9f3bad0fb18a47549da801739ae73e4a
yla139 <yla139.lin@gmail.com>
2018-03-18 22:49:12 -0700

    done part 1

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render() {
-        return <input />;
+
+    // initialize state exists in class based component, using syntax this.state
+    // state = a pure js obj that used to record and to react to javascript event
+    constructor(props) {
+        super(props);
+
+        // term = 'search term', the property we want to update
+        this.state = { term: '' };
     }
+
+
+    render() {
+        // update state with setState()
+        return (
+            <div> 
+                <input
+                 value={this.state.term}
+                 onChange={ event => this.setState({ term: event.target.value })} />
+                 value: { this.state.term }
+            </div>
+        );
+    }
+
+    // event object descrirbe the context of the event occurred
+
 }
 
 export default SearchBar;
