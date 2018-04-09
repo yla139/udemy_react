cabfbc114543e740ef8325fdd1b6a6381f42acb4
yla139 <yla139.lin@gmail.com>
2018-04-08 19:55:34 -0700

    add youtube api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import searchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyCv8RiCcWGymN0TYhqy4jRB3oAXe1-vE2g';
 
-const API_KEY = 'AIzaSyDgJ6hKzLmWGWmDYZSucjGH6KN2nwOQ6Fk';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 // create a new component. This component should product some html
 const App = () => {
