a04a921dc45300a663a4be9ff112263aa3fef7a9
yla139 <yla139.lin@gmail.com>
2018-04-08 22:20:04 -0700

    23

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCv8RiCcWGymN0TYhqy4jRB3oAXe1-vE2g';
 
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
-
 // create a new component. This component should product some html
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        // call api and get video data
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos} />
+        </div>
+        );
+    }
 }
 
 // take this component's generated HTML and put it on the page 
