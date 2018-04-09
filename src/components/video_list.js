812e4dd204601faf76a24e10b225803565e83944
yla139 <yla139.lin@gmail.com>
2018-04-08 22:32:48 -0700

    update jquery version

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            
         </ul>
     );
 };
