c6e06a14c917cc9f8bed137d4c7b0b1793512699
yla139 <yla139.lin@gmail.com>
2018-04-09 22:08:09 -0700

    handling null props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCv8RiCcWGymN0TYhqy4jRB3oAXe1-vE2g';
 
 // create a new component. This component should product some html
@@ -10,6 +11,7 @@ class App extends Component {
     constructor(props) {
         super(props);
 
+        // initialize video
         this.state = { videos: [] };
 
         // call api and get video data
@@ -22,6 +24,7 @@ class App extends Component {
         return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
             <VideoList videos={this.state.videos} />
         </div>
         );
@@ -30,6 +33,3 @@ class App extends Component {
 
 // take this component's generated HTML and put it on the page 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-
-
